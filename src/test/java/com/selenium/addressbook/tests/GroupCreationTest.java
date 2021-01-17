d222f7539e33a840eb6a0deffe7e3114d639b67a
spike <kyiv_qa@ukr.net>
2021-01-17 13:11:35 +0200

    lesson 6, part4, created the DataProvider for testGroupCreation

--- src/test/java/com/selenium/addressbook/tests/GroupCreationTest.java
+++ src/test/java/com/selenium/addressbook/tests/GroupCreationTest.java
@@ -2,7 +2,12 @@ package com.selenium.addressbook.tests;
 
 import com.selenium.addressbook.model.GroupData;
 import com.selenium.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,11 +15,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2").withHeader("test2").withFooter("test2");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
