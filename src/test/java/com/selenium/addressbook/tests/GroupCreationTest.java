2965ff364994560d2b27bf905a98fb9b679b4e2d
spike <kyiv_qa@ukr.net>
2021-01-24 14:09:05 +0200

    lesson 6, part11, added the library for logging and created the logfile configuration

--- src/test/java/com/selenium/addressbook/tests/GroupCreationTest.java
+++ src/test/java/com/selenium/addressbook/tests/GroupCreationTest.java
@@ -5,6 +5,8 @@ import com.google.gson.Gson;
 import com.selenium.addressbook.model.GroupData;
 import com.selenium.addressbook.model.Groups;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
