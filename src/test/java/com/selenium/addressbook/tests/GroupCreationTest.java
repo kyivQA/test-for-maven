153ee7d413249ca4b049356b98bf9a538abefc9e
spike <kyiv_qa@ukr.net>
2021-01-24 10:34:59 +0200

    lesson 6, part6, creating the data in the file XML and changing the DataProvider for testGroupCreation

--- src/test/java/com/selenium/addressbook/tests/GroupCreationTest.java
+++ src/test/java/com/selenium/addressbook/tests/GroupCreationTest.java
@@ -2,6 +2,7 @@ package com.selenium.addressbook.tests;
 
 import com.selenium.addressbook.model.GroupData;
 import com.selenium.addressbook.model.Groups;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -9,6 +10,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,15 +20,17 @@ public class GroupCreationTest extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validGroups() throws IOException {
-        List<Object[]> list = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.csv"));
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.xml"));
+        StringBuilder xml = new StringBuilder();
         String line = reader.readLine();
         while(line != null) {
-            String [] split = line.split(";");
-            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            xml.append(line);
             line = reader.readLine();
         }
-        return list.iterator();
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml.toString());
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
     @Test(dataProvider = "validGroups")
